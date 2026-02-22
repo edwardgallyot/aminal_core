193811dc61a17bb49a2de268eab12be3279c503a
mustardmontinator <mustardmontinator@proton.me>
2026-02-22 19:45:36 +0000

    Iterating over the midi messages in the sampler process

--- src/aminals_sampler.cpp
+++ src/aminals_sampler.cpp
@@ -13,8 +13,15 @@ void Sampler::prepare(double sampleRate, int samplesPerBlock)
 {
 }
 
-void Sampler::process(juce::AudioBuffer<float>& samples, juce::MidiBuffer& buffer)
+void Sampler::process(juce::AudioBuffer<float>& samples, juce::MidiBuffer& midi)
 {
+    for (const auto& metadata : midi)
+    {
+        auto message = metadata.getMessage();
+
+        // For MIDI message debugging
+        // std::cout << message.getDescription() << std::endl;
+    }
 }
 
 void Sampler::release()
