002a20714d663247ba018c5ba0792b7f7cb10807
mustardmontinator <mustardmontinator@proton.me>
2026-03-08 00:42:16 +0000

    Arena update to remove pointless ref count

--- include/aminals_arena.hpp
+++ include/aminals_arena.hpp
@@ -23,9 +23,8 @@ namespace aminals
             {
                 if (!p) return;
                 p->~T();
-                arena->release_object();
             }
-            
+
             Arena* arena;
         };
 
@@ -48,40 +47,32 @@ namespace aminals
         template<class T, class... Args>
         Ptr<T> make_unique(Args&&... args)
         {
-            uint8_t* mem = this->push(sizeof(T), alignof(T));
+            std::byte* mem = this->push(sizeof(T), alignof(T));
             if (!mem) return Ptr<T>(nullptr, Deleter<T>(this));
             T* obj = ::new (mem) T(std::forward<Args>(args)...);
-            this->objects_dispensed++;
             return Ptr<T>(obj, Deleter<T>(this));
         }
 
         void reset()
         {
-            assert(this->objects_dispensed == 0);
             this->count = 0;
         }
 
-        void release_object()
-        {
-            assert(this->objects_dispensed > 0);
-            if (this->objects_dispensed) this->objects_dispensed--;
-        }
-        
     private:
         
-        uint8_t* push(size_t size, size_t align)
+        std::byte* push(size_t size, size_t align)
         {
             size_t r = this->count % align;
             if (r) this->count += (align - r);
 
             if (this->count + size > capacity) return nullptr;
 
-            uint8_t* res = &this->buffer[this->count];
+            std::byte* res = &this->buffer[this->count];
             this->count += size;
             return res;
         }        
                       
-        std::array<uint8_t, capacity> buffer;
+        alignas(8) std::array<std::byte, capacity> buffer;
         size_t count;
         size_t objects_dispensed;
     };
